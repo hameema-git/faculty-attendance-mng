e0de61692df53bd350465d6439fe5466a5ae9241
Hameema1 <hameema.f@beekatechnologies.com>
2025-05-29 20:28:09 +0530

    Add temporary migration trigger view14

--- faculty_attendance/settings.py
+++ faculty_attendance/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = "faculty_attendance.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -96,9 +96,9 @@ DATABASES = {
 #         'PORT': '5432',
 #     }
 # }
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 
 # Password validation
